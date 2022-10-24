5f017a88c792343f68012e425b1ed970a8ff170d
unknown <zahidmd1996@gmail.com>
2022-10-24 23:34:24 +0530

    search

--- src/component/search/search.js
+++ src/component/search/search.js
@@ -5,7 +5,7 @@ export default function Search() {
         <div>
             <form className="d-flex" role="search">
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
+                {/* <button className="btn btn-outline-success" type="submit">Search</button> */}
             </form>
         </div>
     )
