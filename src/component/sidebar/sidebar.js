993035c2c44153eb8a7031077fcea0436f16d87f
unknown <zahidmd1996@gmail.com>
2022-10-30 23:56:02 +0530

    library

--- src/component/sidebar/sidebar.js
+++ src/component/sidebar/sidebar.js
@@ -47,11 +47,42 @@ export default function Sidebar() {
                             </NavLink>
                         </div>
                     </div>
+                    <div className='d-flex justify-content-between align-items-center mt-2 mb-3'>
+                        <div className='d-flex align-items-center'>
+                            <NavLink>
+                                <p className='smartup m-0'>LIBRARY</p>
+                            </NavLink>
+                        </div>
+                    </div>
                     <div className='d-flex justify-content-between align-items-center mb-3'>
                         <div className='d-flex align-items-center'>
                             <NavLink to={'/high-rated'} className="home-wrapper">
                                 <i className="fa-solid fa-house home-img"></i>
-                                <p className='profile m-0'>Famous</p>
+                                <p className='profile m-0'>Recent</p>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className='d-flex justify-content-between align-items-center mb-3'>
+                        <div className='d-flex align-items-center'>
+                            <NavLink to={'/high-rated'} className="home-wrapper">
+                                <i className="fa-solid fa-house home-img"></i>
+                                <p className='profile m-0'>bookmarked</p>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className='d-flex justify-content-between align-items-center mb-3'>
+                        <div className='d-flex align-items-center'>
+                            <NavLink to={'/high-rated'} className="home-wrapper">
+                                <i className="fa-solid fa-house home-img"></i>
+                                <p className='profile m-0'>Top rated</p>
+                            </NavLink>
+                        </div>
+                    </div>
+                    <div className='d-flex justify-content-between align-items-center mb-3'>
+                        <div className='d-flex align-items-center'>
+                            <NavLink to={'/high-rated'} className="home-wrapper">
+                                <i className="fa-solid fa-house home-img"></i>
+                                <p className='profile m-0'>Downloaded</p>
                             </NavLink>
                         </div>
                     </div>
