7102c993d2293a311758ebab66629bd6fa25d1d0
unknown <zahidmd1996@gmail.com>
2022-11-05 23:59:24 +0530

    p

--- src/component/profile/profile.js
+++ src/component/profile/profile.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export default function Profile() {
-  return (
-    <div>
-      <p>Profile section</p>
-    </div>
-  )
+    return (
+        <div>
+            <div class="dropdown">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown link
+                </a>
+
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </div>
+    )
 }
