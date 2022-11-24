571e0cc0345a0509bf427a3e4633cc01d223b78e
unknown <zahidmd1996@gmail.com>
2022-11-24 22:57:58 +0530

    search section

--- src/component/search/search.js
+++ src/component/search/search.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Search() {
     return (
-        <div>
+        <div className='mt-3'>
             <form className="d-flex" role="search">
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                 {/* <button className="btn btn-outline-success" type="submit">Search</button> */}
