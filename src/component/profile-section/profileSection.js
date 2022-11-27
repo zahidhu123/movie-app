8b9820f9295b9db46585a42175152aec8f2f9eee
unknown <zahidmd1996@gmail.com>
2022-11-27 22:40:40 +0530

    profile-s

--- src/component/profile-section/profileSection.js
+++ src/component/profile-section/profileSection.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Profile from '../profile/profile'
+import Search from '../search/search'
 
 export default function ProfileSection() {
   return (
     <div className='p-4'>
       <Profile/>
+      <Search/>
     </div>
   )
 }
