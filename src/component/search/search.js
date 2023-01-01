cf42901a625d89aefe2a6612be11c566d715000b
unknown <zahidmd1996@gmail.com>
2023-01-01 21:55:31 +0530

    search sectionnnn

--- src/component/search/search.js
+++ src/component/search/search.js
@@ -3,7 +3,7 @@ import "./search.css"
 
 export default function Search() {
     return (
-        <div className='mt-3'>
+        <div className='mt-4'>
             <form className="d-flex" role="search">
                 <input className="form-control search-section" type="search" placeholder="Search" aria-label="Search" />
                 {/* <button className="btn btn-outline-success" type="submit">Search</button> */}
