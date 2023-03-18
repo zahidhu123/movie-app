360d638a1411e7aed1c778e608d4b77f972efa36
unknown <zahidmd1996@gmail.com>
2023-03-18 23:39:46 +0530

    popullarmovie section

--- src/component/popular-movies/polularMovies.js
+++ src/component/popular-movies/polularMovies.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getNewsApi } from '../../apiService';
 import Modal from '../modal/modal';
-import '../popular-movies/popularMovies.css'
+// import '../popular-movies/popularMovies.css'
 
 
 export default function PopularMovies(props) {
